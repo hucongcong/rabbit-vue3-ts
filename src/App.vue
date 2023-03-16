3a80f146623213f8494c545786092ee91efb3aa9
hucongcong <flycc258@163.com>
2023-03-16 23:00:31 +0800

    axios的简单封装

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
+<script lang="ts" setup>
+import request from './utils/request'
+const test = async () => {
+  const res = await request.get('/home/index')
+  console.log(res)
+}
+test()
+</script>
+
 <template>
   <div>App根组件</div>
 </template>
