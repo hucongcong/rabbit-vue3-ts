996021c2bb63e8d7e54a06abaf4fc270dbc374ee
hucongcong <flycc258@163.com>
2023-03-19 00:40:58 +0800

    首页布局-头部布局

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup name="Layout">
 import AppTopnav from './components/app-topnav.vue'
+import AppHeader from './components/app-header.vue'
 </script>
 <template>
   <AppTopnav></AppTopnav>
+  <AppHeader></AppHeader>
 </template>
 
 <style lang="less" scoped></style>
