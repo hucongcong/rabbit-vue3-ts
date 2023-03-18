75e2454fa55ef49d306222724c116475e592f6a5
hucongcong <flycc258@163.com>
2023-03-19 00:15:14 +0800

    样式处理-less 变量与 mixins

--- src/App.vue
+++ src/App.vue
@@ -8,5 +8,15 @@ test()
 </script>
 
 <template>
-  <div>App根组件</div>
+  <h1>App根组件</h1>
 </template>
+
+<style lang="less" scoped>
+// 必须导入variables.less
+@import '@/assets/styles/variables.less';
+@import '@/assets/styles/mixins.less';
+h1 {
+  background-color: @warnColor;
+  .hoverShadow();
+}
+</style>
